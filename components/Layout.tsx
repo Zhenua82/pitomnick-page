37f10e05ebaf5e478822da824762f4a78bd11674
Zhenua82 <zzhmenka@yandex.ru>
2025-11-29 18:34:10 +0300

    withoutBasket

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,8 @@
 import React, { useEffect } from 'react';
 import Link from 'next/link';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { updateQuantity, removeItem, clearCart, restoreCart } from '../store/cartSlice';
-import { RootState } from '../store';
-
-
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const dispatch = useDispatch();
-  const items = useSelector((state: RootState) => state.cart.items);
 
-  useEffect(() => {
-    dispatch(restoreCart());
-  }, [dispatch]);
-
-  const total = items.reduce((sum, item) => sum + item.quantity * item.price, 0);
   return (
     <>
       <header className="site-header">
@@ -23,17 +11,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
         <nav className="nav">
             <Link href="/">Главная</Link>
-            <Link href="/cart">Корзина</Link>
-          <div>
-            {items.map((item) => (
-              item.quantity > 0 && (<div key={item.slug + item.age}>
-                  <h5>{item.title}, возраст: {item.age} - {item.quantity} шт.</h5>    
-              </div>)
-            ))}
-          </div>
-          <div >
-            <h3>Итого: {total} ₽</h3>
-          </div>
+            <Link href="#">О нас</Link>
         </nav>
 
         </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Provider } from "react-redux"
-import { store } from "../store/index"
+
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
+  return ( 
       <Component {...pageProps} />
-    </Provider>
   )
 }
 
