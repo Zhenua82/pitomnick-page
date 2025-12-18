d9a3d2a2834c703c63c4c65a70959526236138ec
Zhenua82 <zzhmenka@yandex.ru>
2025-12-18 13:17:54 +0300

    githubPages

---
+++ next.config.js
@@ -0,0 +1,41 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   trailingSlash: true,
+
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'i.imgur.com',
+//       },
+//     ],
+//   },
+
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const repo = 'pitomnick-page';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'i.imgur.com' }
+    ],
+  },
+
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
