7275a58e8cc5ec76984fe708280b03b1454401ce
Zhenua82 <zzhmenka@yandex.ru>
2026-06-01 15:58:55 +0300

    fix btn

--- components/phoneButton.tsx
+++ components/phoneButton.tsx
@@ -3,7 +3,7 @@ import styles from './phoneButton.module.css'
 export default function PhoneButton(){
     return (
         <div className={styles.buttonContainer}>
-            <a href="tel:+78910991929" className={styles.phoneButton}></a>
+            <a href="tel:+79110991929" className={styles.phoneButton}></a>
             <span>Имеются вопросы, звоните!</span>
         </div>
     )
